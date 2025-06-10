f66f90584062fe32b410f4e7f65b8f1681ef4de6
Rina Vas <rinavas.2001@gmail.com>
2025-06-10 14:49:12 +0100

    modules refactored

--- src/components/views/Modules.jsx
+++ src/components/views/Modules.jsx
@@ -1,111 +1,77 @@
-import {CardContainer, Card} from "../UI/Card.jsx"
-import './Modules.scss';
+import { useState, useEffect } from "react";
+import { CardContainer, Card } from "../UI/Card.jsx";
+import "./Modules.scss";
 
 function Modules(){
 
-  const modulelist = [
-    {
-      ModuleID: 1,
-      ModuleName: "Games Programming",
-      ModuleCode: "CI2270",
+  // Initialisation ------------------------------
+  const newModule = {
+      ModuleID: Math.floor(10000 * Math.random()),
+      ModuleName: "New Module",
+      ModuleCode: "CI9999",
       ModuleLevel: 4,
-      ModuleLeaderID: 1,
-      ModuleImageURL: "https://images.freeimages.com/images/small-previews/9b8/electronic-components-2-1242738.jpg"
-    },
-    {
-      ModuleID: 2,
-      ModuleName: "Individual Project",
-      ModuleCode: "CI7446",
-      ModuleLevel: 7,
-      ModuleLeaderID: 2,
-      ModuleImageURL: "https://images.freeimages.com/images/small-previews/411/light-of-technology-1510575.jpg"
-    },
-    {
-      ModuleID: 3,
-      ModuleName: "Database Driven Web Applications",
-      ModuleCode: "CI6388",
-      ModuleLevel: 6,
-      ModuleLeaderID: 3,
-      ModuleImageURL: "https://images.freeimages.com/images/small-previews/64b/vla-1-1315506.jpg"
-    },
-    {
-      ModuleID: 4,
-      ModuleName: "Advanced Data Modelling",
-      ModuleCode: "CI8502",
-      ModuleLevel: 6,
-      ModuleLeaderID: 4,
-      ModuleImageURL: "https://images.freeimages.com/images/small-previews/293/cable-4-1243085.jpg"
-    },
-    {
-      ModuleID: 5,
-      ModuleName: "Ethical Hacking",
-      ModuleCode: "CI7572",
-      ModuleLevel: 7,
-      ModuleLeaderID: 5,
-      ModuleImageURL: "https://images.freeimages.com/images/small-previews/fa1/cable-5-1243077.jpg"
-    },
-    {
-      ModuleID: 6,
-      ModuleName: "Network and Mobile Forensics",
-      ModuleCode: "CI5068",
-      ModuleLevel: 7, 
-      ModuleLeaderID: 6, 
-      ModuleImageURL: "https://images.freeimages.com/images/small-previews/930/towertv-3-1423238.jpg"
-    },
-    {
-      ModuleID: 7,
-      ModuleName: "Practical Data Analyst Skills",
-      ModuleCode: "CI9213",
-      ModuleLevel: 5,
-      ModuleLeaderID: 7,
-      ModuleImageURL: "https://images.freeimages.com/images/small-previews/cf5/cellphone-1313194.jpg"
-    },
-    {
-      ModuleID: 8,
-      ModuleName: "Statistics in Practice",
-      ModuleCode: "CI5856",
-      ModuleLevel: 7,
-      ModuleLeaderID: 8,
-      ModuleImageURL: "https://images.freeimages.com/images/small-previews/4e8/sala-de-parto-03-1432033.jpg"
-    },
-    {
-      ModuleID: 9,
-      ModuleName: "Internet Services and Protocols",
-      ModuleCode: "CI3651",
-      ModuleLevel: 7,
-      ModuleLeaderID: 9,
-      ModuleImageURL: "https://images.freeimages.com/images/small-previews/6cc/monitor-2-1242535.jpg"
-    },
-    {
-      ModuleID: 10,
-      ModuleName: "High Level Game Development",
-      ModuleCode: "CI7952",
-      ModuleLevel: 5,
       ModuleLeaderID: null,
-      ModuleImageURL: "https://images.freeimages.com/images/small-previews/402/rocket-in-the-museum-1450195.jpg"
-    }
-  ];
+      ModuleImageURL: "https://images.freeimages.com/images/small-previews/9b8/electronic-components-2-1242738.jpg",
+    };
+    const apiURL = "https://softwarehub.uk/unibase/api";
+    const modulesEndpoint = `${apiURL}/modules`;
+  // State ---------------------------------------
+    const [modules, setModules] = useState(null);
 
-    return(
+    // API Fetch -----------------------------------
+    const apiGet = async (endpoint) => {
+      try {
+        const response = await fetch(endpoint);
+        const result = await response.json();
+        setModules(result);
+      } catch (error) {
+        console.error("Error fetching modules:", error);
+        setModules([]);
+      }
+    };
+
+    // Effect --------------------------------------
+    useEffect(() => {
+      apiGet(modulesEndpoint);
+    }, [modulesEndpoint]);
+
+    // Handlers ------------------------------------
+    const handleAdd = (module) => {
+      setModules([...modules, module]);
+      console.log(`Length of modules: ${modules.length}`);
+    };
+
+    // View ----------------------------------------
+    return (
+    <>
+      <h1>Modules</h1>
+      {!modules ? (
+        <p>Loading records ...</p>
+      ) : modules.length === 0 ? (
+        <p>No records found.</p>
+      ) : (
         <>
-            <h1>Modules</h1>
-            <CardContainer>
-            {
-                modulelist.map((module)=>{
-                    return(
-                    <div className='moduleCard' key={module.ModuleCode}>
-                    <Card>
-                        <p>{module.ModuleCode}</p>
-                        <p>{module.ModuleName}</p>
-                        <img src={module.ModuleImageURL}/>
-                    </Card>
-                    </div>
-                )
-                })
-            }
-            </CardContainer>
+          <CardContainer>
+            {modules.map((module) => {
+              return (
+                <div className="moduleCard" key={module.ModuleCode}>
+                  <Card>
+                    <p>{module.ModuleCode}</p>
+                    <p>{module.ModuleName}</p>
+                    <img
+                      src={module.ModuleImageURL}
+                      alt={module.ModuleCode}
+                    />
+                  </Card>
+                </div>
+              );
+            })}
+          </CardContainer>
+          <button onClick={() => handleAdd(newModule)}>Add module</button>
         </>
-    );
+      )}
+    </>
+  );
 }
 
 export default Modules;
